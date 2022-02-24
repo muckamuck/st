ed92ea2b5ac3e21834e5c6742d8cf1909e3408e2
Duke Takle <duke.takle@protonmail.com>
2022-02-23 22:48:20 -0600

    yellow

--- config.h
+++ config.h
@@ -123,7 +123,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
-	"gray90", /* default foreground colour */
+	"#cdcd00", /* default foreground colour */
 	"black", /* default background colour */
 };
 
